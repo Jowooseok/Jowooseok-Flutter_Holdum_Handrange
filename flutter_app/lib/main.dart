dcd46c502823b8b0b7c7707a9705f7b88c644288
Jowooseok <whdntjrsla12@naver.com>
2020-09-17 19:24:01 +0900

    GridvIEW 테스트

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,6 +4,18 @@ void main() {
   runApp(MyApp());
 }
 
+String cardIndex(int num) {
+  String hands = '';
+  if (num == 1) {
+    hands = 'AA';
+  } else if (num == 2) {
+    hands = 'KK';
+  } else if (num == 3) {
+    hands = 'QQ';
+  }
+  return hands;
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -31,23 +43,83 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Holdum_HandRange'),
+        title: Text('Joeun_HandRange'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: [
-                Container(
-                  child: Text('노커밋'),
-                  color: Colors.indigoAccent,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(3),
+        crossAxisSpacing: 2,
+        mainAxisSpacing: 2,
+        crossAxisCount: 13,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text('AA'),
+            color: Colors.teal[100],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[200],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[300],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[400],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[500],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('AA'),
+            color: Colors.teal[600],
+          ),
+        ],
+      )),
     );
   }
 }
