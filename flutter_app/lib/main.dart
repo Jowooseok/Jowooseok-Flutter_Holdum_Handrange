6f47c78e451e993f0626df5f4b5f6c0e55d2fde8
Jowooseok <whdntjrsla12@naver.com>
2020-09-19 17:53:51 +0900

    포지션 클릭시 코지션 색깔 바뀌는것 이외에 전역변수 바운더리 바뀌도록 설정

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 
+int boundary = 169; //경계선
+
 void main() {
   runApp(MyApp());
-}
-
-String cardIndex(int num) {
-  String hands = '';
-  if (num == 1) {
-    hands = 'AA';
-  } else if (num == 2) {
-    hands = 'KK';
-  } else if (num == 3) {
-    hands = 'QQ';
-  }
-  return hands;
 }
 
 class MyApp extends StatelessWidget {
@@ -37,8 +27,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> item = ['AA', 'KK', 'AK', 'AKs'];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,9 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [Text('hi')],
-            ),
+            PositionButtons(),
             Row(
               children: [
                 Flexible(
@@ -62,24 +48,19 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisSpacing: 2,
                     crossAxisCount: 13,
                     children: [
-                      Container(
-                        padding: const EdgeInsets.all(1),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          "AA",
-                          style: TextStyle(),
-                        ),
-                        color: Colors.teal[100],
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(1),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          "AAs",
-                          style: TextStyle(),
-                        ),
-                        color: Colors.teal[100],
-                      ),
+                      Cards(1, 'AA'),
+                      Cards(4, 'AKs'),
+                      Cards(6, 'AQs'),
+                      Cards(8, 'AJs'),
+                      Cards(12, 'ATs'),
+                      Cards(19, 'A9s'),
+                      Cards(24, 'A8s'),
+                      Cards(30, 'A7s'),
+                      Cards(34, 'A6s'),
+                      Cards(28, 'A5s'),
+                      Cards(32, 'A4s'),
+                      Cards(33, 'A3s'),
+                      Cards(39, 'A2s'),
                     ],
                   ),
                 )
@@ -98,14 +79,225 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class PositionButtons extends StatefulWidget {
+  @override
+  _PositionButtonsState createState() => _PositionButtonsState();
+}
+
+class _PositionButtonsState extends State<PositionButtons> {
+  int utgColor = 0xffBDBDBD;
+  int utg1Color = 0xffBDBDBD;
+  int utg2Color = 0xffBDBDBD;
+  int loJackColor = 0xffBDBDBD;
+  int hiJackColor = 0xffBDBDBD;
+  int cutOffColor = 0xffBDBDBD;
+  int buttonColor = 0xffBDBDBD;
+  int sbbbColor = 0xffBDBDBD;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: FlatButton(
+            color: Color(utgColor),
+            child: Text('UTG'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xff616161;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 17;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(utg1Color),
+            child: Text('UTG1'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xff616161;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 17;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(utg2Color),
+            child: Text('UTG2'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xff616161;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 22;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(loJackColor),
+            child: Text('LJ'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xff616161;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 27;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(hiJackColor),
+            child: Text('HJ'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xff616161;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 34;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(cutOffColor),
+            child: Text('CO'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xff616161;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 44;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(buttonColor),
+            child: Text('BT'),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xff616161;
+                sbbbColor = 0xffBDBDBD;
+              });
+              boundary = 68;
+            },
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Color(sbbbColor),
+            child: Text(
+              'SBBB',
+            ),
+            onPressed: () {
+              setState(() {
+                utgColor = 0xffBDBDBD;
+                utg1Color = 0xffBDBDBD;
+                utg2Color = 0xffBDBDBD;
+                loJackColor = 0xffBDBDBD;
+                hiJackColor = 0xffBDBDBD;
+                cutOffColor = 0xffBDBDBD;
+                buttonColor = 0xffBDBDBD;
+                sbbbColor = 0xff616161;
+              });
+              boundary = 80;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class Cards extends StatefulWidget {
+  int index = 0;
+  String cards = '';
+
+  Cards(int index, String cards) {
+    this.index = index;
+    this.cards = cards;
+  }
+
   @override
   _CardsState createState() => _CardsState();
 }
 
 class _CardsState extends State<Cards> {
+  int cardcolor = 0xffbdbdbd;
+
+  A() => {
+        if (boundary >= widget.index)
+          {
+            setState(() {
+              cardcolor = 0xff616161;
+            })
+          }
+        else
+          {
+            setState(() {
+              cardcolor = 0xffbdbdbd;
+            })
+          }
+      };
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    A();
+    return Container(
+      padding: const EdgeInsets.all(1),
+      alignment: Alignment.center,
+      child: Text(
+        widget.cards,
+      ),
+      color: Color(cardcolor),
+    );
   }
 }
